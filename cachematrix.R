aa23167b1b7ce6b7bdba234e815916508868789e
kev8k <kevin.gedik@students.fhnw.ch>
2025-10-26 15:14:56 +0100

    Added final working sachematrix solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+rm(list = ls(all.names = T))
+getwd()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) { # 1. Set the matrix
+    x <<- y
+    inv <<- NULL   # clear previous inverse
+  }
+  
+  get <- function() x # 2. Get the matrix
+  setInverse <- function(inverse) inv <<- inverse # 3. Set the inverse
+  getInverse <- function() inv# 4. Get the inverse
+  list(set = set,  # Return all 4 as a list
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {# If inverse already cached
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()# Otherwise compute it
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+##test the function
+
+m <- matrix(c(2, 3, 2, 4), 2, 2)
+cm<-makeCacheMatrix(m)
+
+cacheSolve(cm)
+cm$set(matrix(c(1,2,3,4),2,2))
+cacheSolve(cm)
+
+
